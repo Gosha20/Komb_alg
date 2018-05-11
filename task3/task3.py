11f5c15e191be8c3ebf9ae1184e6f761c6b16945
Gosha20 <gosha3548@gmail.com>
2018-05-11 21:57:05 +0500

    end

--- task3/task3.py
+++ task3/task3.py
@@ -1,6 +1,3 @@
-import copy
-import sys
-import copy
 
 n = 0
 point_from = 0
@@ -12,7 +9,7 @@ def read_file():
     global n
     global point_from
     global point_to
-    with open('in.txt', 'r', encoding='utf-8') as file:
+    with open('in2.txt', 'r', encoding='utf-8') as file:
         n = int(file.readline().rstrip("\n"))
         for i in range(n):
             matrix.append([int(x) for x in file.readline().rstrip('\n').split()])
@@ -20,13 +17,13 @@ def read_file():
         point_to = int(file.readline().rstrip('\n')) - 1
 
 
-def write_file(path,max_weight):
+def write_file(_path, max_weight):
     with open('out.txt', 'w', encoding='utf-8') as file:
-        if len(result_path) is not 0:
+        if len(_path) is not 0:
             file.write("Y\n")
             result = ''
-            for i in path:
-                result += str(i)+ " "
+            for i in _path:
+                result += str(i+1)+ " "
             file.write(result.rstrip() + '\n')
             file.write(str(max_weight))
         else:
@@ -37,43 +34,44 @@ def dijkstra(N, S, matrix):
     valid = [True] * N
     weight = [1000000] * N
     weight[S] = 0
+    parent = [S]*(N)
+    parent[S] = -1
     for i in range(N):
         min_weight = 1000001
         ID_min_weight = -1
-        # ыбирается минимальная метка вершины из не посещенных
+        # бирается мин метка вершины из не посещенных
         for i in range(N):
             if valid[i] and weight[i] < min_weight:
                 min_weight = weight[i]
                 ID_min_weight = i
         for i in range(N):
             if matrix[ID_min_weight][i] != -32768:
-                if weight[ID_min_weight] + matrix[ID_min_weight][i] < weight[i]:
-                    weight[i] = weight[ID_min_weight] + matrix[ID_min_weight][i]
+                if weight[i] > max(weight[ID_min_weight], matrix[ID_min_weight][i]):
+                    weight[i] = max(weight[ID_min_weight], matrix[ID_min_weight][i])
+                    parent[i] = ID_min_weight
             valid[ID_min_weight] = False
-    return weight
+    return weight, [x for x in parent]
 
 
-def find_path(point_from, point_to, matrix, weight):
+
+def find_path(point_from, point_to, parents):
     path = []
     point_to = point_to
-    path.append(point_to)
-    while weight[point_to] < 1000000:
-        for j in range(len(weight)):
-            if matrix[j][point_to] != -32768:
-                if weight[point_to] - matrix[j][point_to] == weight[j]:
-                    path.append(j)
-                    point_to = j
-                    break
-        if point_from == point_to:
-            break
-    if len(path) != 1:
+    if parents[point_to] < 1000000:
+        path.append(point_to)
+        v = point_to
+        while v != point_from:
+            w = parents[v]
+            path.append(w)
+            v = w
         path = path[::-1]
-        max_w = max([matrix[path[x]][path[x+1]] for x in range(len(path)-1)])
-        return [x+1 for x in path], max_w
-    return [], 0
+        return path
+    return []
+
+
 
 read_file()
-w = dijkstra(n, point_from, matrix)
-result_path, max_w = find_path(point_from, point_to, matrix, w)
-write_file(result_path, max_w)
+w, perents = dijkstra(n, point_from, matrix)
+path = find_path(point_from, point_to, perents)
+write_file(path, w[point_to])
 
